3512278d1ffb2e0abc6725b9188cea1e10b434e9
KinandaKrisandika <kinandakrisandikanurseto@gmail.com>
2023-12-21 22:38:11 +0700

    creat a python file

---
+++ latihan.py
@@ -0,0 +1,9 @@
+number : int = 12
+number_2 : float = 30.5
+boolean_var = True
+string_var = "My string"
+
+if __name__ == '__main__':
+    print(number + number_2)
+    print(number + boolean_var)
+    print(number + string_var)
